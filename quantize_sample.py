30092658fe6be3c5d04f2330707850f9a9238ec2
Xiaobo Zhao <zxbnpu@gmail.com>
2025-04-10 22:05:50 +0200

    Correct directory creation calls in quantize_sample.py from mkdirs to makedirs for proper functionality

--- quantize_sample.py
+++ quantize_sample.py
@@ -61,9 +61,9 @@ def main():
     args.selection_batch = args.batch
     print(args)
     if args.save_path != "" and not os.path.exists(args.save_path):
-        os.mkdirs(args.save_path, exist_ok=True)
+        os.makedirs(args.save_path, exist_ok=True)
     if not os.path.exists(args.data_path):
-        os.mkdirs(args.data_path)
+        os.makedirs(args.data_path, exist_ok=True)
 
     # conduct non-overlapping coreset selection for multiple times
     for exp in range(args.num_exp):
