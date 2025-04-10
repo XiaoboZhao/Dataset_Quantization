e24b055bb79bf21d7b99ce7bd3647b9e40a89754
Xiaobo Zhao <zxbnpu@gmail.com>
2025-04-10 21:58:08 +0200

    Ensure directory creation is safe by using exist_ok=True in quantize_sample.py

--- quantize_sample.py
+++ quantize_sample.py
@@ -61,7 +61,7 @@ def main():
     args.selection_batch = args.batch
     print(args)
     if args.save_path != "" and not os.path.exists(args.save_path):
-        os.mkdir(args.save_path)
+        os.mkdir(args.save_path, exist_ok=True)
     if not os.path.exists(args.data_path):
         os.mkdir(args.data_path)
 
